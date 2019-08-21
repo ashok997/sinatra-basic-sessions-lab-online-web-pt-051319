005eeb10092ba07d397331567a8c89c5e5a9a2ab
ASHOK MAHARJAN <ashok997@gmail.com>
2019-08-21 17:59:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,6 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
+  configure do
+  enable :sessions
+  set :session_secret, "secret"
+  
+end
+  
 get '/'
    @session = session
   :index
